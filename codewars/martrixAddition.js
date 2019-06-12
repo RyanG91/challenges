943cd0fe29790eff5809c5cbaf04041971a71f37
Ryan Grosser <ryan.grosser91@gmail.com>
2019-06-12 15:30:59 +1000

    solved

--- codewars/martrixAddition.js
+++ codewars/martrixAddition.js
@@ -1,7 +1,4 @@
-// Revisit and make sure you understand this.
-
 function matrixAddition(a, b){
-  //TODO
   total = []
   total = new Array(a.length)
   for (let i = 0; i < total.length; i++) {
@@ -11,9 +8,6 @@ function matrixAddition(a, b){
     }
   }
   return total
-
-  // Would work if it wasnt an array inside an array.
-  // a.forEach((a, i) => total.push(a + b[i]))
 }
 
 matrixAddition([[1,2],[3,4]], [[2,3],[4,5]])
